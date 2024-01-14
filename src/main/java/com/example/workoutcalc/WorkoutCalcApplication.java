f0cf37317b4caeeeea0a4839a0cdc3307e836cf4
WoodallTechServices <dwoodall328@gmail.com>
2024-01-13 20:05:19 -0600

    Took database connection out of main application file. Caused azure deploy error ,and took out unused imports

--- src/main/java/com/example/workoutcalc/WorkoutCalcApplication.java
+++ src/main/java/com/example/workoutcalc/WorkoutCalcApplication.java
@@ -1,12 +1,11 @@
 package com.example.workoutcalc;
 
-import com.example.workoutcalc.DataBase.DBUtil;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-import java.sql.Connection;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
 import java.sql.SQLException;
 
 @SpringBootApplication
@@ -14,11 +13,11 @@ import java.sql.SQLException;
 public class WorkoutCalcApplication {
     public static void main(String[] args) throws SQLException {
 
-        Connection con = DBUtil.getConnection();
-
-        if (con != null) {
-            System.out.println("Database connected");
-        }
+//        Connection con = DBUtil.getConnection();
+//
+//        if (con != null) {
+//            System.out.println("Database connected");
+//        }
 
         SpringApplication.run(WorkoutCalcApplication.class, args);
     }
